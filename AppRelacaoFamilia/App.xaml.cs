0229e62d7d42e6d9b0a9fca5b4835be1ec6a736e
Arthur Cesar de Freitas <Arthurfreitas2006@hotmail.com>
2025-04-12 20:08:25 -0300

    Adicionar arquivos de projeto.

---
+++ AppRelacaoFamilia/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppRelacaoFamilia
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
